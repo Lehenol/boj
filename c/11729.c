b598be807ded48904a34214b59140c0ea2b140b0
saenozu <dawn1504@naver.com>
2021-12-08 03:37:17 +0900

    2021-12-07

---
+++ c/11729.c
@@ -0,0 +1,27 @@
+//11729_하노이의 탑 이동 순서
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+// n개의 판을 from 기둥에서 to 기둥으로 옮긴다
+void solve(int n, int from, int to) {
+	if (n == 0) return; // 남은 판 없으면 종료
+
+	int remain = 6 - from - to; // 기둥(1,2,3) 합이 6이므로
+
+	solve(n - 1, from, remain);
+	printf("%d %d\n", from, to);
+	solve(n - 1, remain, to);
+}
+
+int main() {
+	int n;
+	scanf("%d", &n);
+
+	int k = (int)pow(2, n) - 1; // 이동 횟수 : n 일때 2^n - 1
+
+	printf("%d\n", k);
+	solve(n, 1, 3);
+
+	return 0;
+}
